02d1fe042ea7f06d1cf6651dc0ae5157349a7ff3
unknown <zhurui1351@gmail.com>
2017-05-28 22:41:40 +0800

    adjust some functions

--- src/strategy/shock/nbar.R
+++ src/strategy/shock/nbar.R
@@ -2,7 +2,7 @@ source('src/config/include.R',encoding='utf-8')
 sourceDir('src/dw/interface')
 source('src/dw/collectdata/collectfromwind.R')
 sourceDir('src/algorithm')
-source('src/strategy/shock/nbarclass')
+source('src/strategy/shock/nbarclass.R')
 
 
 nbarframework = function()
@@ -14,15 +14,21 @@ nbarframework = function()
   orgin_data = getdata(dbname,tbname,freq)
   data = orgin_data
   
+  data$sma = lag(SMA(data$Close,20),1)
+  data = na.omit(data)
+  
   nbarstate = NbarState$new()
-  position = Position$new()
+  position = Position$new()  
+  
+  winpoint = 20
+  losspoint = 15
   
   for(i in 1:nrow(data))
   {
     #print(i)
     d = data[i,]
     
-    position = nbar_strategy(d,position,nbarstate)
+    position = nbar_strategy(d,position,nbarstate,losspoint=losspoint,winpoint=winpoint)
     position$update(d,nbarstate)
     nbarstate$update(d)   
   }
--- src/strategy/shock/nbarclass.R
+++ src/strategy/shock/nbarclass.R
@@ -59,27 +59,28 @@ NbarState = R6Class('nbarstate',
                       
                       ))
 
-nbar_strategy = function(d,position,nbarstate)
+nbar_strategy = function(d,position,nbarstate,losspoint=10,winpoint=10)
 {
-  point = 5
+  point = 10
   time = as.character(index(d))
   open = as.numeric(d$Open)
+  sma = as.numeric(d$sma)
   
-  if(nbarstate$upcount == 3)
+  if(nbarstate$upcount == 3 )
   {
     #openbuy()
-    stoploss = open - point
-    stopwin = open + point + 5
+    stoploss = open - losspoint 
+    stopwin = open + winpoint 
     r = data.frame(opentime=time,closetime=NA,open=open,close=NA,stopwin=stopwin,stoploss=stoploss,type='long',exittype='')
     trade = Trade$new(r,stopwin=defaultstopwin,stoploss=defaultstoploss)
     position$add(trade)
   }
   
-  if(nbarstate$downcount ==3)
+  if(nbarstate$downcount ==3 )
   {
     #opensell()
-    stoploss = open + point
-    stopwin = open - point - 5
+    stoploss = open + losspoint 
+    stopwin = open - winpoint
     r = data.frame(opentime=time,closetime=NA,open=open,close=NA,stopwin=stopwin,stoploss=stoploss,type='short',exittype='')
     trade = Trade$new(r,stopwin=defaultstopwin,stoploss=defaultstoploss)
     position$add(trade)
